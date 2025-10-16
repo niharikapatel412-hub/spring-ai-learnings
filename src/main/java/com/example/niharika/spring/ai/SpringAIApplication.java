a1ffe0b732b50ff267bf7c9c92f096ecac0f0507
niharikap1 <niharikapatel412@gmail.com>
2025-10-16 16:07:50 +0100

    Initial setup

---
+++ src/main/java/com/example/niharika/spring/ai/SpringAIApplication.java
@@ -0,0 +1,15 @@
+package com.example.niharika.spring.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example.niharika.spring.ai.chat"})
+public class SpringAIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAIApplication.class, args);
+	}
+
+}
